b5561c283e6d335a6a9d796f65a3efdd10c66f4a
amabellecarvalho <amabelle.carvalho@gmail.com>
2025-12-28 20:51:07 -0300

    Imprimindo agencia e conta do cliente

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = scanner.nextInt();
+
+        // Limpando o buffer do teclado
+        scanner.nextLine();
+
+        scanner.close();
     }
 }
